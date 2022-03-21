03c29fbf12a1fe6e10a7f91bd4777c7898e5ac2c
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-03-20 23:40:33 -0500

    Flipped data and options order

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const https = require('https');
 
 const channels = {
-    Death: "https://discord.com/api/webhooks/954896573861597245/ZPqd5xwcx_AFo9OcsYDxqHHqHjafT75IyluMqzJqbaL5dydS6ItGB_goEq5wlDdobZjw",
-    Grip: "https://discord.com/api/webhooks/954907659750998086/O7OWdtC-8cIg7Wnr6_CC6zNVt879fBBP9GxJ3sJI-wkovJII54NPoaRokdwzebPoD5xa"
+    Death: "/api/webhooks/954896573861597245/ZPqd5xwcx_AFo9OcsYDxqHHqHjafT75IyluMqzJqbaL5dydS6ItGB_goEq5wlDdobZjw",
+    Grip: "/api/webhooks/954907659750998086/O7OWdtC-8cIg7Wnr6_CC6zNVt879fBBP9GxJ3sJI-wkovJII54NPoaRokdwzebPoD5xa"
 }
 
 const requestListener = function (req, res) {
@@ -12,17 +12,7 @@ const requestListener = function (req, res) {
     if (url.startsWith("/dis")) {
         // nil, "dis", channel, 
         const args = url.split("/")
-
-        const options = {
-            hostname: 'discord.com',
-            port: 443,
-            path: '/todos',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Content-Length': data.length
-            }
-        }
+        const channel = args[3]
 
         const data = {
             username: botname,
@@ -40,6 +30,17 @@ const requestListener = function (req, res) {
                     }
                 }
             ]
+        }
+
+        const options = {
+            hostname: 'discord.com',
+            port: 443,
+            path: channels[channel],
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': data.length
+            }
         }
 
     }
