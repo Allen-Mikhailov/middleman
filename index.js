b6fcf5422a242341495b9faffde5f5cb7def9b30
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-03-21 18:54:16 -0500

    Working Version maybe

--- index.js
+++ index.js
@@ -1,55 +1,34 @@
-const https = require('http');
+const http = require('http');
+var request = require('request');
 
 const channels = {
-    Death: "/api/webhooks/954896573861597245/ZPqd5xwcx_AFo9OcsYDxqHHqHjafT75IyluMqzJqbaL5dydS6ItGB_goEq5wlDdobZjw",
-    Grip: "/api/webhooks/954907659750998086/O7OWdtC-8cIg7Wnr6_CC6zNVt879fBBP9GxJ3sJI-wkovJII54NPoaRokdwzebPoD5xa"
+	Death: "https://discord.com/api/webhooks/954896573861597245/ZPqd5xwcx_AFo9OcsYDxqHHqHjafT75IyluMqzJqbaL5dydS6ItGB_goEq5wlDdobZjw",
+	Grip: "https://discord.com/api/webhooks/954907659750998086/O7OWdtC-8cIg7Wnr6_CC6zNVt879fBBP9GxJ3sJI-wkovJII54NPoaRokdwzebPoD5xa"
 }
 
 const requestListener = function (req, res) {
     const url = req.url
     const botname = "SERVER"
 
+    console.log(url)
+
     if (url.startsWith("/dis")) {
+        console.log("Request")
         // nil, "dis", channel, header, subheader, footer
         const args = url.split("/");
         const channel = args[3];
-        const header = args[4];
-        const subheader = args[5];
-        const footer = args[6];
+        const content = args[4];
 
-        const data = JSON.stringify({
-            username: botname,
-            embeds: [
-                {
-                    fields: [
-                        {
-                            name: header,
-                            value: subheader
-                        }
-                    ],
-                    color: 16518199,
-                    footer: {
-                        text: footer
-                    }
-                }
-            ]
-        })
+        const data = {["content"]: content}
 
-        const options = {
-            hostname: 'discord.com',
-            port: 443,
-            path: channels[channel],
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Content-Length': data.length
-            }
-        }
-
-        const reqe = https.request(options);
-        reqe.write(data);
-        // console.log(data)
-        reqe.end();
+        request({
+            url: channels[channel],
+            method: "POST",
+            json: true,   // <--Very important!!!
+            body: data
+        }, function (error, response, body){
+            console.log("Message Sent");
+        });
 
         res.writeHead(200);
         res.end("Success");
@@ -59,5 +38,5 @@ const requestListener = function (req, res) {
     }
 }
 
-const server = https.createServer(requestListener);
+const server = http.createServer(requestListener);
 server.listen(8080);
